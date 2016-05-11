86a700debdc57fdea4fa3e1a488f4feced4fbcd8
wumingyu <317808023@qq.com>
2016-05-11 21:47:40 +0800

    flex布局

--- js/app.js
+++ js/app.js
@@ -2,25 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Bucket from './bucket/bucket.js'
 
-const styles = {
-  contain:{
-    width:'100%',
-    height:'100%',
-    display:'flex',
-    justifyContent:'space-around',
-    alignItems:'stretch',
-    //flexDirection:'column',
-    //background:'red'
-  },
-  jiange:{
-    display:'flex',
-    flexGrow:'1',
-    borderColor:'green',
-    bordrWidth:'2px',
-    borderStyle:'solid'
-  }
-
-};
 
 
 let App = React.createClass({
@@ -28,15 +9,51 @@ let App = React.createClass({
     return (
       <div style={styles.contain}>
         <div style={styles.jiange}></div>
-        <div style={styles.jiange}><Bucket></Bucket></div>
+        <div style={styles.bucket}>
+          <Bucket capacity={3} watercapacity={2}></Bucket>
+        </div>
         <div style={styles.jiange}></div>
-        <div style={styles.jiange}><Bucket></Bucket></div>
+        <div style={styles.bucket}>
+          <Bucket capacity={5} watercapacity={2}></Bucket>
+        </div>
         <div style={styles.jiange}></div>
-        <div style={styles.jiange}><Bucket></Bucket></div>
+        <div style={styles.bucket}>
+          <Bucket capacity={8} watercapacity={2}></Bucket>
+        </div>
         <div style={styles.jiange}></div>
       </div>
     );
   }
 });
 
+const styles = {
+  contain:{
+    width:'100%',
+    height:'100%',
+    display:'flex',
+    alignItems:'stretch',
+    //flexDirection:'column',
+    //background:'red'
+  },
+  jiange:{
+    display:'flex',
+    flex:1,
+    borderColor:'green',
+    bordrWidth:'2px',
+    borderStyle:'solid',
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+  bucket:{
+    display:'flex',
+    flex:2,
+    borderColor:'green',
+    bordrWidth:'2px',
+    borderStyle:'solid',
+    alignItems:'center',
+    justifyContent: 'center',
+    position:'relative'
+  }
+
+};
 ReactDOM.render(<App/>,document.getElementById("app"));
--- js/bucket/bucket.js
+++ js/bucket/bucket.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const HEIGHT_TO_TEN=60;//10升水对应的vh高度
+
 
 
 export default class bucket extends React.Component {
@@ -10,15 +12,59 @@ export default class bucket extends React.Component {
 
   render() {
     return (
-      <div style={styles.bucket}></div>
+      <div style={styles.contain}>
+        <div style={styles.title}>{this.props.watercapacity+'升'+'/'+this.props.capacity+'升'}</div>
+        {/*height 代表水桶的高度，waterPercent代表桶里面有百分多少的水*/}
+        <div style={Object.assign({},styles.bucket,{height:this.props.capacity/10*HEIGHT_TO_TEN+'vh'})}>
+          <div style={
+            Object.assign(
+              {},
+              styles.nowater,
+              {flex:this.props.capacity-this.props.watercapacity}
+              )}>
+            
+          </div>
+          <div style={
+            Object.assign(
+              {},
+              styles.water,
+              {flex:this.props.watercapacity}
+              )}>
+          </div>
+        </div>
+      </div>
     );
   }
 }
-
 var styles={
-	bucket:{
-		height:'50%',
-		width:'10%',
-		background:'red'
-	}
+  contain:{
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'stretch',
+    flex:'0 0 80%'
+  },
+  title:{
+    display:'flex',
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  bucket:{
+    display:'flex',
+    flexDirection:'column',
+    background:'lightgrey',
+    justifyContent:'center',
+    alignItems:'stretch'
+  },
+  nowater:{
+    display:'flex',
+    flex:1,
+    justifyContent:'center'
+  },
+  water:{
+    display:'flex',
+    flex:1,
+    background:'lightblue'
+  }
 }
+
